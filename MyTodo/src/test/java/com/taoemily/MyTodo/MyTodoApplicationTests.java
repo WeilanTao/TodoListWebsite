43440f3c4757a16ffd36f1be50f72ca85d9f4bc9
WeilanTao <64599029+WeilanTao@users.noreply.github.com>
2021-07-15 09:46:33 -0400

    first commit

---
+++ MyTodo/src/test/java/com/taoemily/MyTodo/MyTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taoemily.MyTodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
